c3c9baeb7f953a429678afc21908d753f3df3539
Hector <hectorpena6590@gmail.com>
2025-06-22 00:21:58 -0700

    rotated boxes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="relative max-sm:scale-[0.75] max-sm:origin-center max-sm:p-6">
-      {/* Mobile rotated boxes  */}
+      {/* Rotated boxes  */}
       <div className="absolute inset-0 flex items-center justify-center md:hidden z-0">
         <div className="w-[350px] h-[350px] border border-dotted border-[#A0A4AB] rotate-45 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
         <div className="w-[420px] h-[420px] border border-dotted border-[#A0A4AB] rotate-45 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
