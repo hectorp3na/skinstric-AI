9c0b77db17a303127e90daa928d17511c9a6d290
Hector <hectorpena6590@gmail.com>
2025-06-25 14:28:50 -0700

    fixing hover

--- src/components/MainText.jsx
+++ src/components/MainText.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 
-const MainText = ({ hoverDirection }) => {
+const MainText = ({
+  title = "Sophisticated",
+  subtitle = "skincare",
+  mobileDescription,
+  desktopDescription,
+  hoverDirection,
+}) => {
   let transform;
   if (hoverDirection === "left") {
     transform = "translateY(20%) translateX(100px)";
   } else if (hoverDirection === "right") {
     transform = "translateY(20%) translateX(-100px)";
   } else {
-    transform = "translateY(20%)";
+    transform = "translateY(0%)";
   }
 
   return (
@@ -18,34 +24,30 @@ const MainText = ({ hoverDirection }) => {
         className="relative z-10 text-center transition-all duration-500"
       >
         <h1
-          style={{
-            transform,
-            transition: "transform 0.3s ease",
-          }}
+          style={{ transform, transition: "transform 0.3s ease" }}
           className="text-[60px] text-[#1A1B1C] lg:text-[100px] font-inter font-normal tracking-tighter leading-none"
         >
-          Sophisticated
+          {title}
           <br />
-          <span className="block text-[#1A1B1C]">skincare</span>
+          <span className="block text-[#1A1B1C]">{subtitle}</span>
         </h1>
       </div>
 
       {/* Mobile Paragraph */}
-      <p className="z-10 block lg:hidden w-[30ch] mt-4 text-[16px] font-semibold text-center text-[#1a1b1c83]">
-        Skinstric developed an A.I. that creates a highly-personalized routine
-        tailored to what your skin needs.
-      </p>
-
-      {/* Bottom Paragraph */}
-      <div className="hidden lg:block fixed bottom-[calc(-7vh)] left-[calc(-20vw)] xl:left-[calc(-27vw)] 2xl:left-[calc(-31vw)] font-normal text-sm text-[#1A1B1C] space-y-3 uppercase">
-        <p>
-          Skinstric developed an A.I. that creates a<br />
-          highly-personalized routine tailored to
-          <br />
-          what your skin needs.
+      {mobileDescription && (
+        <p className="z-10 block lg:hidden w-[30ch] mt-4 text-[16px] font-semibold text-center text-[#1a1b1c83]">
+          {mobileDescription}
         </p>
-      </div>
+      )}
+
+      {/* Desktop Paragraph */}
+      {desktopDescription && (
+        <div className="hidden lg:block fixed bottom-[calc(-7vh)] left-[calc(-20vw)] xl:left-[calc(-27vw)] 2xl:left-[calc(-31vw)] font-normal text-sm text-[#1A1B1C] space-y-3 uppercase">
+          <p>{desktopDescription}</p>
+        </div>
+      )}
     </>
   );
 };
+
 export default MainText;
--- src/components/SquareBoxes.jsx
+++ src/components/SquareBoxes.jsx
@@ -5,11 +5,11 @@ const SquareBoxes = () => {
         <div className="absolute flex items-center justify-center lg:hidden z-0 -translate-x-1 -translate-y-1/4">
               <div className="relative w-[420px] h-[420px] -translate-x-1 -translate-y-1">
                 <div
-                  style={{ transform: "translateY(10%) translateX(10%)" }}
+                  style={{ transform: "translateY(5%) translateX(5%)" }}
                   className="absolute w-[450px] h-[450px] inset-0 border border-dotted border-[#A0A4AB] rotate-45 -translate-x-1 -translate-y-[10%]"
                 />
                 <div
-                  style={{ transform: "translateY(4%) translateX(4%)" }}
+                  style={{ transform: "translateY(8%) translateX(8%)" }}
                   className="absolute top-1/2 left-1/2 w-[350px] h-[350px] border border-dotted border-[#A0A4AB] rotate-45 -translate-x-1/2 -translate-y-1/2"
                 />
               </div>
