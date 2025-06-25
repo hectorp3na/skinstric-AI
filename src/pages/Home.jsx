c4258f25092dd054e2d8b357e96140ce2f58d638
Hector <hectorpena6590@gmail.com>
2025-06-25 01:21:53 -0700

    adding hover effect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
+  const [isHovered, setIsHovered] = useState(false);
   return (
     <>
       <main className="antialiased text-[#1A1B1C]">
@@ -25,7 +26,11 @@ const Home = () => {
               id="main-heading"
               className="relative z-10 text-center transition-all duration-500"
             >
-              <h1 className="text-[60px] text-[#1A1B1C] lg:text-[100px] font-inter font-normal tracking-tighter leading-none">
+              <h1 style={{ transform: isHovered 
+                ? "translateY(20%) translateX(100px)"
+              : "translateY(20%",
+            transition: "transform 0.3s ease",
+           }} className="text-[60px] text-[#1A1B1C] lg:text-[100px] font-inter font-normal tracking-tighter leading-none">
                 Sophisticated
                 <br />
                 <span className="block text-[#1A1B1C]">skincare</span>
@@ -72,12 +77,18 @@ const Home = () => {
 
             {/* CTA Left */}
             <div
-              style={{ transform: "translateY(20%)" }}
+              style={{ transform: isHovered 
+                ? "translateY(20%) translateX(-100px)"
+              : "translateY(20%",
+            transition: "transform 0.3s ease",
+           }}
               className="hidden lg:block fixed left-[calc(-53vw)] xl:left-[calc(-50vw)] top-1/2 -translate-y-1/2 w-[500px] h-[500px]"
             >
               <div className="relative w-full h-full">
                 <div className="w-full h-full border border-dotted border-[#A0A4AB] rotate-45 fixed inset-0" />
                 <button
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
                   id="discover-button"
                   className="
     inline-flex items-center justify-center gap-4 whitespace-nowrap rounded-md text-sm text-[#1A1B1C]
@@ -99,13 +110,19 @@ const Home = () => {
 
             {/* CTA Right */}
             <div
-              style={{ transform: "translateY(20%)" }}
+            style={{ transform: isHovered 
+                ? "translateY(20%) translateX(100px)"
+              : "translateY(20%",
+            transition: "transform 0.3s ease",
+           }}
               className="hidden lg:block fixed right-[calc(-53vw)] xl:right-[calc(-50vw)] top-1/2 -translate-y-1/2 w-[500px] h-[500px]"
             >
               <div className="relative w-full h-full">
                 <div className="w-full h-full border border-dotted border-[#A0A4AB] rotate-45 absolute inset-0" />
                 <a href="/testing">
                   <button
+                     onMouseEnter={() => setIsHovered(true)}
+                     onMouseLeave={() => setIsHovered(false)}
                     id="take-test-button"
                     className="inline-flex items-center gap-4 text-sm text-[#1A1B1C] h-9 absolute top-1/2 left-0 -translate-y-1/2 -translate-x-1/5 xl:-translate-x-1/6 px-3 py-1 cursor-pointer group-hover:scale-[0.92] ease duration-300 hover:text-black"
                   >
