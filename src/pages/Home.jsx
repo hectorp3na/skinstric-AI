5c6914beeaafc3dce90cd529b30196299dcdcf29
Hector <hectorpena6590@gmail.com>
2025-06-25 10:24:09 -0700

    adjusting boxes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,11 +11,11 @@ const Home = () => {
             <div className="absolute flex items-center justify-center lg:hidden z-0 -translate-x-1 -translate-y-1/4">
               <div className="relative w-[420px] h-[420px] -translate-x-1 -translate-y-1">
                 <div
-                  style={{ transform: "translateY(5%) translateX(5%)" }}
+                  style={{ transform: "translateY(10%) translateX(10%)" }}
                   className="absolute w-[450px] h-[450px] inset-0 border border-dotted border-[#A0A4AB] rotate-45 -translate-x-1 -translate-y-[10%]"
                 />
                 <div
-                  style={{ transform: "translateY(15%) translateX(15%)" }}
+                  style={{ transform: "translateY(4%) translateX(4%)" }}
                   className="absolute top-1/2 left-1/2 w-[350px] h-[350px] border border-dotted border-[#A0A4AB] rotate-45 -translate-x-1/2 -translate-y-1/2"
                 />
               </div>
