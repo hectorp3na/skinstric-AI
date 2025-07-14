cc9e12ef3c4353cae8247449f45d251d146d3cf0
Hector <hectorpena6590@gmail.com>
2025-07-13 23:10:02 -0700

    LAst working Vite state

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { reactRouter } from "@react-router/dev/vite";
-import react from '@vitejs/plugin-react';
 import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+
 import tsconfigPaths from "vite-tsconfig-paths";
-import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [
-    tailwindcss(),
-    reactRouter(),
+    react(),
     tsconfigPaths(),
-    react()
+    
   ],
 });
